04d7afd1d03ae0b252469f3c5dc1a962d2998381
strlghf <strlghf@gmail.com>
2026-06-15 11:19:40 -0400

    error handler

---
+++ back/src/middlewares/errorHandler.ts
@@ -0,0 +1,20 @@
+import type { Request, Response, NextFunction } from "express";
+
+export function errorHandler(
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const statusCode = err.statusCode || (res.statusCode !== 200 ? res.statusCode : 500);
+
+  const responseBody = {
+    success: false,
+    message: err.message || "An unexpected error ocurred",
+    ...(process.env.NODE_ENV !== "production") && { stack: err.stack }
+  }
+
+  console.error(`[Error Handler]`, err);
+
+  return res.status(statusCode).json(responseBody);
+}
